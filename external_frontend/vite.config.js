7149946148f9fbd690811ac326183d089957850f
Chirag-agg <ca.aggarwal2006@gmail.com>
2026-02-18 06:53:50 +0530

    api endpoints fixed

--- external_frontend/vite.config.js
+++ external_frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+                    'chart-vendor': ['recharts'],
+                    'map-vendor': ['react-simple-maps', 'cobe'],
+                    'animation-vendor': ['framer-motion', 'gsap']
+                }
+            }
+        }
+    }
 })
